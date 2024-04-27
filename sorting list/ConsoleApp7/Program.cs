d7925772016b5e12363af44c8aa3885a4a4d1aab
Martin Ristov <155776503+martinristow@users.noreply.github.com>
2024-04-28 01:47:45 +0200

    Add files via upload

---
+++ sorting list/ConsoleApp7/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>();
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine("Vnesi element vo listata "+ (i+1).ToString());
+                lista.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine();
+            }
+            int broj = lista.Count;
+            bool dali = lista.Contains(5);
+            
+            if(dali==true) 
+            {
+                int pozicija = lista.IndexOf(5);
+                Console.WriteLine("Brojot e " + pozicija); 
+            }
+            else
+            {
+                Console.WriteLine("Brojot ne postoi!"); 
+            }
+            Console.WriteLine();
+            lista.RemoveAt(9);
+            lista.Reverse();
+            Console.WriteLine("Prepravena lista : ");
+            for(int i=0;i<9;i++)
+            {
+                Console.WriteLine(lista[i] +" ");
+
+            }
+            Console.WriteLine();
+            lista.Sort();
+            Console.WriteLine("Sortirana lista : ");
+            for(int i=0;i<9;i++)
+            {
+                Console.WriteLine(lista[i]+" ");
+            }
+            lista.Insert(2, 100);
+            lista.Insert(9, 10);
+            int max = lista[0];
+            for(int i=1;i<10;i++)
+            {
+                if (lista[i] > max)
+                
+                    max = lista[i];
+                
+               
+
+            }
+            Console.WriteLine("Najgolemiot e : " + max);
+
+            Console.ReadKey();
+           
+           }
+        
+    }
+}
---
+++ stack/ConsoleApp16/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stek = new Stack<int>();//deklariranje na stek
+            stek.Push(1);//Vnesuvanje na elementi vo stek
+            stek.Push(2);
+            stek.Push(3);
+            stek.Push(4);
+            stek.Push(5);
+            Console.WriteLine("Sodrzi " + stek.Count());//broe kolku elem ima vnatre
+            Console.WriteLine("Struktura na stekot : ");
+            foreach(int i in stek) Console.WriteLine(i);//Pecatenje na site elementi 
+            Console.WriteLine("Vrvot na stekot e : " + stek.Peek());//Prviot element vo magacinot
+            int vred =stek.Pop();//Brise ga vrvo na stekot
+            if (vred == 5)//Proverka dali izbrisaniot vrv na stekot e deliv so 5
+            {
+                Console.WriteLine("Deliv");
+            }
+            else Console.WriteLine("Ne");
+            if (stek.Contains(4))//proveruvanje na brojo 4 dali e vnatre vo stekot 
+            {
+                Console.WriteLine("Da ");
+            }
+            else
+                Console.WriteLine("Ne");
+            stek.Clear();//osloboduvanje na memorijata 
+            Console.ReadKey();
+        }
+    }
+}
