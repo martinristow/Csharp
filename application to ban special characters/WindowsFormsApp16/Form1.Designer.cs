d09e384d6c07799d2f27c6b74124494607c79bd4
Martin Ristov <155776503+martinristow@users.noreply.github.com>
2024-04-28 01:29:29 +0200

    Add files via upload

---
+++ application to ban special characters/WindowsFormsApp16/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApp16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(374, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "KOPCE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 139);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 53);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ application to ban special characters/WindowsFormsApp16/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp16
+{
+    public partial class Form1 : Form
+    {
+        private bool broj = true;//go postavuvame na true
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.Cyan;//Postavuvanje na boja na samata forma
+            label1.Text="Vnesi tekst , koj ke sodrzi samo bukvi (brojki i specijalni znaci se zabraneti) !";//labela so tekst
+            label1.Font = new Font("Arial", 10);//Menuvanje na fontot i goleminata na tekstot od labelata
+            label1.ForeColor = Color.Black;//bojata na tekstot od labelata
+            textBox1.BackColor = Color.White;//pozadinskata boja na samiot textbox
+            label2.BackColor = Color.OrangeRed;
+            label2.Text = "MARTIN RISTOV - 102620";
+            label2.Font = new Font("Tahoma", 14);
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)//TextBox 
+        {
+            if (broj == true)//se proveruva dali promenlivata broj ima true vrednost , ako e vistina togas 
+
+            {
+                e.Handled = true;//ova znaci deka site broevi i del od specijalnite znaci se zabranuvaat 
+            }
+            if (e.KeyChar == '+' || e.KeyChar == '-' || e.KeyChar == '*' || e.KeyChar == '/' || e.KeyChar == '-' || e.KeyChar == '=' || e.KeyChar == '`' || e.KeyChar == '.' || e.KeyChar == ',' || e.KeyChar == ';' || e.KeyChar == ']' || e.KeyChar == '[' || e.KeyChar == ':' || e.KeyChar == '<' || e.KeyChar == '>' || e.KeyChar == '?' || e.KeyChar == '"' || e.KeyChar == '|' || e.KeyChar == '{' || e.KeyChar == '}' || e.KeyChar == '_' || e.KeyChar == '+' || e.KeyChar == '~')//dokolku e pretisnato nekoj od ovie znaci , toa znachi deka nema da dozvoli nivno vnesuvanje
+            {
+                e.Handled = true;//ova znaci deka ovie znaci shto gi navedovme vo pogorniot if ke bidat zabraneti
+            }
+
+
+            //********DRUG NACIN ZA ZABRANA NA SPECIJALNITE ZNACI *******
+
+            //if (char.IsControl(e.KeyChar)==false && char.IsDigit(e.KeyChar)==false && char.IsLetter(e.KeyChar)==false && e.KeyChar != ' ')
+            //{
+            //    e.Handled = true;
+            //}
+            //else
+            //{ e.Handled = false; }
+        }
+
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)//TextBox
+        {
+            broj = true;//promenlivata broj ja postavuvame na true vrednost
+            if(e.KeyCode<Keys.D0||e.KeyCode>Keys.D9)//proveruvame dali e stistanato nekakvo kopce vo opseg pomegju broevite od 
+                //do 9 od normalnata tastatura i dokolku e stistanoto istoto nema da bide registrirano vo textboxot
+            {
+                if(e.KeyCode<Keys.NumPad0||e.KeyCode>Keys.NumPad9)//proverka dali e stisnato nekakvo kopce od numerickata tastatura
+                {
+                    broj = false;
+                }
+               
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)//Kopce
+        {
+            int broj = textBox1.Text.Length;
+            if (broj < 1)
+            {
+                
+                MessageBox.Show("Vnesete zbor ili recenica","Prepravi",buttons:MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+               
+            }
+            else {
+                MessageBox.Show("Dolzinata na zborot e " + textBox1.TextLength.ToString() + " i vneseniot zbor e " + textBox1.Text,"Aplikacija",buttons:MessageBoxButtons.OK);//Prikazuvanje na tekstot so ShowBox kade sto vo nego ke imame informacii za dolzinata na vneseniot zbor i koj zbor sme go vnesile
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
