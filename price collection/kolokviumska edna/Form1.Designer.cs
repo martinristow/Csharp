0acf1da13cd9f78d37eec3a0db7db4d60094197b
Martin Ristov <155776503+martinristow@users.noreply.github.com>
2024-04-28 01:23:37 +0200

    Add files via upload

---
+++ price collection/kolokviumska edna/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace kolokviumska_edna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.presmetajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.presmetajToolStripMenuItem,
+            this.startToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(406, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Koktel $2",
+            "Mango $5",
+            "Portokali $4",
+            "limuni $3",
+            "borovnica $8"});
+            this.listBox1.Location = new System.Drawing.Point(349, 215);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(234, 180);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // presmetajToolStripMenuItem
+            // 
+            this.presmetajToolStripMenuItem.Name = "presmetajToolStripMenuItem";
+            this.presmetajToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.presmetajToolStripMenuItem.Text = "Presmetaj";
+            this.presmetajToolStripMenuItem.Click += new System.EventHandler(this.presmetajToolStripMenuItem_Click);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem presmetajToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+    }
+}
+
---
+++ price collection/kolokviumska edna/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kolokviumska_edna
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+            label1.Text = "Rezultat !";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        public void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        public void presmetajToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //int cena;
+            //int brojac = 0;
+            //int vkupna_cena = 0;
+            //for(int i = 0;i<listBox1.Items.Count;i++)
+            //{
+            //    if (listBox1.GetSelected(i))
+            //    {
+            //        brojac++;
+            //        string proizvod = listBox1.Items[i].ToString();
+            //        string[] podeli = proizvod.Split('$');
+            //        if (int.TryParse(podeli[1], out cena) )
+            //        {
+            //            vkupna_cena += cena;
+            //            if(brojac==1)
+            //            {
+            //                label1.Text = "Selektiravte " + brojac + " proizvod i toj cini " + vkupna_cena;
+            //            }
+            //            else
+            //            {
+            //                label1.Text = "Selektiravte " + brojac + " proizvodi i tie cinat " + vkupna_cena;
+            //            }
+
+            //        }
+
+
+            //    }
+            //}
+            int cena;
+            int brojac=0;
+            int vkupna_cena=0;
+            for(int i=0;i<listBox1.Items.Count;i++)
+            {
+                if(listBox1.GetSelected(i))
+                {
+                    brojac++;
+                    string zemi = listBox1.Items[i].ToString();
+                    string[] podeli=zemi.Split('$');
+                    if (int.TryParse(podeli[1],out cena))
+                    {
+                        vkupna_cena += cena;
+                        label1.Text = "Selek " + brojac + " cena : " + vkupna_cena;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Greska Vnes!");
+                    }
+                    
+                }
+            }
+           
+        }
+    }
+}
+
