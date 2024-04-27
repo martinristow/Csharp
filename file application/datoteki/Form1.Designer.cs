9d349e3a74b0dd1b2e958cf8884bed66d41e3698
Martin Ristov <155776503+martinristow@users.noreply.github.com>
2024-04-28 01:26:14 +0200

    Add files via upload

---
+++ file application/datoteki/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace datoteki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(637, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(571, 157);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(209, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(34, 72);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(34, 109);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(95, 20);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Location = new System.Drawing.Point(21, 157);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(144, 100);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(21, 56);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(103, 20);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(21, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(21, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(21, 17);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(103, 20);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(485, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(488, 278);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(289, 109);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(160, 56);
+            this.trackBar1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ file application/datoteki/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace datoteki
+{
+    public partial class Form1 : Form
+    {
+        List<string> lista = new List<string> { "O da da", "Partizane", " najboli si ti" };
+        public Form1()
+        {
+            //trackBar1.TickStyle =TickStyle.TopLeft;
+            //trackBar1.Minimum = 0;
+            //trackBar1.Maximum = 100;
+            //trackBar1.TickFrequency = 1/2;
+            InitializeComponent();
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+            panel1.Visible = false;
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+            radioButton3.Visible = false;
+            radioButton4.Visible = false;
+            checkBox1.Text = "VIP1";
+            checkBox2.Text = "T-MOBILE";
+            button1.Text = "PRODOLZI";
+            button1.Size = new System.Drawing.Size(80, 50);
+            label1.Text = "IZBERETE JA FILIJALATA KADE STO STE CLEN!";
+            label1.Font = new System.Drawing.Font("Arial", 8,System.Drawing.FontStyle.Bold);
+            listBox1.Visible = false;
+            comboBox1.Visible = false;
+            label2.Visible = false;
+            
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Otvori belo lence vrata (*.txt)|*.txt";
+            openFileDialog.FilterIndex = 0;
+            openFileDialog.Title = "Otvori me be";
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string datotekicka=openFileDialog.FileName;
+                try
+                {
+                    //string[] lines = File.ReadAllLines(datotekicka);
+                    //comboBox1.Items.Clear();
+                    //comboBox1.Items.AddRange(lines);
+                    using (StreamReader sr = new StreamReader(datotekicka))
+                    {
+                        string[] dodeligu =File.ReadAllLines(datotekicka);
+                        comboBox1.Items.AddRange(dodeligu);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Greska" + ex.Message);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+           
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Datoteka(*.txt)|*.txt";
+            saveFileDialog.FilterIndex = 0;
+            saveFileDialog.Title = "Martin";
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string datoteka=saveFileDialog.FileName;
+                try
+                {
+                    using(StreamWriter da=new StreamWriter(datoteka))
+                    {
+                        for (int i = 0; i < listBox1.Items.Count; i++)
+                        {
+                            if (listBox1.GetSelected(i) == true)
+                            {
+                                string selektirani = listBox1.Items[i].ToString();
+                                da.Write(selektirani+"\n");
+                            }
+                        }
+                       
+                    }
+                    Process.Start("notepad.exe", datoteka);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //ZNACI PRVO PROVERUVAMO SO INDEX 0 I EDEN KOJ ELEMENT E IZBRANO OD LISTBOXO
+            //POSLE ZNACI AKO E IZBRANO SO INDEX 0 ELEMENTITE GI POKAZUVAMO U COMBOBOX ISTO I OD INDEX1 I TAKA POSLE GI PRAVEMO 
+            //DRUGITE POTREBNI OPERACII
+            if (listBox1.SelectedIndex == 0)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    MessageBox.Show("Vnesete tip na pretplata");
+                }
+                else
+                {
+                    MessageBox.Show("Vnesete mala pretplata");
+                }
+            }
+            else
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    List<string> list = new List<string>();
+                    label2.Visible = true;
+                    label2.Text = "Vnesete go imeto i prezimeto na korisnikot!";
+                    string[] iminja = { "Martin Ristov", "Filip Tane", "Tane Natasha", "Natasha Ristova" };
+                    list.AddRange(iminja);
+                    string ime_od_korisnik=textBox1.Text;
+                    string[] proveri_korisnik=ime_od_korisnik.Split(' ');
+
+                    if ((int.TryParse(proveri_korisnik[0], out int broj)) || (int.TryParse(proveri_korisnik[1], out int broj1)))
+                    {
+                        MessageBox.Show("VNESOVTE I BROJKI A TREBA SAMO TEKST");
+                    }
+                    else
+                    {
+                        if(list.Contains(textBox1.Text))
+                        {
+                            MessageBox.Show("Korisnikot " + textBox1.Text + " e uspesno odstranet");
+                           list.RemoveAt(textBox1.Text.Length);
+                            foreach(string s in list)
+                            {
+                                MessageBox.Show("Ostanati se" + s);
+                            }
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("Takov korisnik ne postoi");
+                        }
+                        //  iminja.Contains == textBox1.Text.ToString();
+                        
+                       
+                    }
+
+                }
+                   // MessageBox.Show("Korisnikot e uspesno izbrisan");
+                }
+            }
+           
+
+
+            //if(comboBox1.Text=="Martin")
+            //{
+            //    listBox1.SelectedItem=null;
+            //    listBox1.SelectedIndex = 0;
+            //}
+            //switch(listBox1.SelectedIndex)
+            //{
+            //    case 0:
+            //        comboBox1.SelectedIndex = 0; MessageBox.Show("Vnesete pogolema pretplata");
+            //        //listBox1.SelectedItem = null; 
+
+            //        break;
+            //    case 1://listBox1.SelectedItem = null; 
+            //        comboBox1.SelectedIndex = 1;MessageBox.Show("Da");
+            //        break;
+            //    case 2:
+            //       // listBox1.SelectedItem = null;
+            //        comboBox1.SelectedIndex = 2; MessageBox.Show("DA");
+            //        break;
+            //        default: //listBox1.SelectedItem = null;
+            //                 listBox1.SelectedIndex = 3; break;
+            //}
+            //if (comboBox1.SelectedIndex == 0)
+            //{
+
+            //}
+
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.CheckState==CheckState.Checked)
+            {
+                checkBox2.Checked = false;
+                listBox1.Items.Clear();
+            }
+           
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.CheckState == CheckState.Checked)
+            {
+                checkBox1.Checked = false;
+                listBox1.Items.Clear();
+            }
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //foreach(string elementi in lista)
+            //{
+            //    MessageBox.Show(elementi+"\n");
+            //}
+            for(int i=0;i<lista.Count;i++)
+            {
+                MessageBox.Show(lista[i]);
+            }
+            if (checkBox1.CheckState == CheckState.Checked)
+            {
+                panel1.Visible = true;
+                radioButton1.Visible = true;
+                radioButton2.Visible = true;
+                radioButton3.Visible = false;
+                radioButton4.Visible = false;
+                radioButton1.Text = "Pretplata";
+                radioButton2.Text = "Telefon";
+                
+                //  listBox1.Visible = true;
+                //comboBox1.Visible = true;
+                // MessageBox.Show("Cao mali " + checkBox1.Text);
+            }
+            else if(checkBox2.CheckState == CheckState.Checked)
+            {
+                radioButton1.Visible = false;
+                radioButton2.Visible = false;
+                panel1.Visible = true;
+                radioButton3.Visible = true;
+                radioButton4.Visible = true;
+                radioButton3.Text = "Telefon";
+                radioButton4.Text = "Pretplata";
+                
+                // listBox1.Visible = true;
+                // comboBox1.Visible = true;
+                //  MessageBox.Show("Cao mali " + checkBox2.Text);
+            }
+            else
+            {
+                MessageBox.Show("Mora da izberete nekoj od ponudenite opcii","Greska",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                panel1.Visible = false;
+                radioButton1.Visible = false;
+                radioButton2.Visible = false;
+                radioButton3.Visible = false;
+                radioButton4.Visible = false;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked==true)
+            {
+                listBox1.Visible = true;
+                comboBox1.Visible = true;
+                listBox1.BackColor = Color.IndianRed;
+                string[] iminja = { "Azuriraj pretplata", "Otstrani pretplatnik" };
+                listBox1.SelectionMode = SelectionMode.One;
+                listBox1.Items.AddRange(iminja);
+              
+
+            }
+            else
+            {
+                listBox1.Items.Clear();
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                listBox1.Visible = true;
+                comboBox1.Visible = true;
+                listBox1.BackColor = Color.IndianRed;
+                listBox1.Items.Clear();
+                string[] iminja = { "Ipad", "Telefon" };
+                listBox1.SelectionMode = SelectionMode.One;
+                listBox1.Items.AddRange(iminja);
+            }
+            else
+            {
+                listBox1.Items.Clear();
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked == true)
+            {
+                listBox1.Visible = true;
+                comboBox1.Visible = true;
+                listBox1.BackColor = Color.Cyan;
+                
+                string[] iminja = { "Promeni pretplata", "Otkazi pretplata" };
+               listBox1.SelectionMode = SelectionMode.One;
+                listBox1.Items.AddRange(iminja);
+               
+            }
+            else
+            {
+                listBox1.Items.Clear();
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked == true)
+            {
+                listBox1.Visible = true;
+                comboBox1.Visible = true;
+                listBox1.BackColor = Color.Cyan;
+                string[] iminja = { "Telefon", "Ipad" };
+                listBox1.SelectionMode = SelectionMode.One;
+                listBox1.Items.AddRange(iminja);
+                
+            }
+            else
+            {
+                listBox1.Items.Clear();
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            if (listBox1.SelectedIndex == 0)
+            {
+                string[] raboti = { "Pogolema pretplata", "Pomala pretplata" };
+                comboBox1.Items.AddRange(raboti);
+            }
+            else if(listBox1.SelectedIndex == 1)
+            {
+                string[] raboti = { "Izbrisi korisnik" };
+                comboBox1.Items.AddRange(raboti);
+                
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //if (char.IsDigit(e.KeyChar)||char.IsPunctuation(e.KeyChar))
+            //{ e.Handled = true; }
+        }
+    }
+}
